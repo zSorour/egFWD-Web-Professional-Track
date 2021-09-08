5b0f329658f9e113ab8c270d3b97ec34a421e3c6
Ahmad Sorour <ahmad_sorour@outlook.com>
2021-09-08 12:52:27 +0200

    Setup Node Express Server on port 3000

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -1,19 +1,26 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
+// Port
+const port = 3000;
+
 // Require Express to run server and routes
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+const server = app.listen(port, () => {
+  console.log(`Listening to localhost:${port}`);
+});
