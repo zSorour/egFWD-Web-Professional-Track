d2d3d072799212cd48a9c454c4cd1ebd29224c4f
Ahmad Sorour <ahmad_sorour@outlook.com>
2021-09-10 15:38:44 +0200

    Feat: Added client communication with both OpenWeatherMap and local server using fetch. TODO: Input validation

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -20,6 +20,21 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static("website"));
 
+// GET route that returns projectData object
+app.get("/data", (req, res) => {
+  console.log(projectData);
+  res.send(JSON.stringify(projectData));
+});
+
+// POST route that adds incoming data to the global projectData object
+app.post("/data", (req, res) => {
+  const data = req.body;
+  projectData.temp = data.temp;
+  projectData.date = data.date;
+  projectData.userResponse = data.userResponse;
+  res.send("Data added successfully.");
+});
+
 // Setup Server
 const server = app.listen(port, () => {
   console.log(`Listening to localhost:${port}`);
--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -1,5 +1,62 @@
 /* Global Variables */
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+// A date constant to store today's date
+const date = new Date().toLocaleDateString();
+
+// A constant to store my OpenWeatherMap's API Key
+const API_KEY = "96f9137b3ebc18e13dc2b0d3723fc541";
+
+// A constant to store the base url used throughout the app
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+const fetchWeatherTemp = async (baseURL, zip, apiKey) => {
+  const url = `${baseURL}?zip=${zip}&appid=${apiKey}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+const postWeatherData = async (url, data) => {
+  const response = fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+  return response;
+};
+
+const updateEntry = async (url) => {
+  const response = await fetch(url);
+  const data = response.json().then((data) => {
+    document.querySelector("#date").innerHTML = data.date;
+    document.querySelector("#temp").innerHTML = data.temp;
+    document.querySelector("#content").innerHTML = data.userResponse;
+  });
+};
+
+const onClickHandler = () => {
+  const zipCode = document.querySelector("#zip").value;
+  const feelings = document.querySelector("#feelings").value;
+
+  fetchWeatherTemp(BASE_URL, zipCode, API_KEY)
+    .then((data) => {
+      console.log(data);
+      return postWeatherData("/data", {
+        temp: data.main.temp,
+        date: date,
+        userResponse: feelings
+      });
+    })
+    .then(() => {
+      updateEntry("/data");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// Register event listener to the Generate Button
+document.querySelector("#generate").addEventListener("click", onClickHandler);
