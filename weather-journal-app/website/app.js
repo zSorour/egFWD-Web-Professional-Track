37f0f230f9949f712ddbe199f3adc5d7ee072826
Ahmad Sorour <ahmad_sorour@outlook.com>
2021-09-11 17:48:35 +0200

    Added &units=metric in the fetch url and removed kelvinToCelsius function

--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -9,11 +9,7 @@ const API_KEY = "96f9137b3ebc18e13dc2b0d3723fc541";
 // A constant to store the base url used throughout the app
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
-// Helper function to convert Kelvin to Celsius
-const kelvinToCelsius = (degInKelvin) => {
-  return (degInKelvin - 273.15).toFixed(2);
-};
-
+// A function to validate inputs
 const validateInputs = (zip, feelings) => {
   if (zip === "") {
     alert("ZIP Code cannot be empty!");
@@ -30,7 +26,7 @@ const validateInputs = (zip, feelings) => {
 
 // An asynchronus function that sends an HTTP GET request to OpenWeatherMap API to fetch the current temperature of a location given a zip code.
 const fetchWeatherTemp = async (baseURL, zip, apiKey) => {
-  const url = `${baseURL}?zip=${zip}&appid=${apiKey}`;
+  const url = `${baseURL}?zip=${zip}&appid=${apiKey}&units=metric`;
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -62,9 +58,8 @@ const postWeatherData = async (url, data) => {
 const updateEntry = async (url) => {
   const response = await fetch(url);
   const data = response.json().then((data) => {
-    const tempInCels = kelvinToCelsius(data.temp);
     document.querySelector("#date").innerHTML = data.date;
-    document.querySelector("#temp").innerHTML = `${tempInCels} Celsius`;
+    document.querySelector("#temp").innerHTML = `${data.temp} Celsius`;
     document.querySelector("#content").innerHTML = data.userResponse;
   });
 };
