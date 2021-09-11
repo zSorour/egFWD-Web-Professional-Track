b5332d360df88e9bc554d2960c815bdfcb8c6e8c
Ahmad Sorour <ahmad_sorour@outlook.com>
2021-09-11 13:20:41 +0200

    Updated UI, added validation, and modified readme file

--- weather-journal-app/README.md
+++ weather-journal-app/README.md
@@ -1,10 +1,39 @@
-# Weather-Journal App Project
+# Weather Journal App
 
-## Overview
-This project requires you to create an asynchronous web app that uses Web API and user data to dynamically update the UI. 
+## Table of Contents
+
+- [Weather Journal App](#weather-journal-app)
+  - [Table of Contents](#table-of-contents)
+  - [Description](#description)
+  - [Author](#author)
+  - [Technologies Used](#technologies-used)
+  - [Project Dependencies](#project-dependencies)
+  - [Instructions](#instructions)
+
+## Description
+
+The project is implemented as the second assessment of the [Udacity](https://www.udacity.com/) Front End Nanodegree offered in collaboration with [ITIDA](https://itida.gov.eg/English/Pages/default.aspx) and [egFWD](https://egfwd.com/) as a free scholarship.
+The focus of the project is to practise working with both external third party APIs and simple ones we create using NodeJS and Express, and how the page content can be modified dybamically.
+
+## Author
+
+Ahmad Abdulaziz Sorour.
+
+## Technologies Used
+
+1. HTML
+2. CSS
+3. JavaScript
+4. NodeJS and Express
+
+## Project Dependencies
+
+- NodeJS must be installed alongside NPM.
+- NPM Packages:
+  - express
+  - body-parser
+  - cors
 
 ## Instructions
-This will require modifying the `server.js` file and the `website/app.js` file. You can see `index.html` for element references, and once you are finished with the project steps, you can use `style.css` to style your application to customized perfection.
 
-## Extras
-If you are interested in testing your code as you go, you can use `tests.js` as a template for writing and running some basic tests for your code.
+To open and view the website, open the terminal / cmd in the root folder of the project and run 'node server.js'. Once the server is running, you can simply go to the browser and go to: [localhost:3000](http://localhost:3000)
--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -9,11 +9,39 @@ const API_KEY = "96f9137b3ebc18e13dc2b0d3723fc541";
 // A constant to store the base url used throughout the app
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
+// Helper function to convert Kelvin to Celsius
+const kelvinToCelsius = (degInKelvin) => {
+  return (degInKelvin - 273.15).toFixed(2);
+};
+
+const validateInputs = (zip, feelings) => {
+  if (zip === "") {
+    alert("ZIP Code cannot be empty!");
+    return false;
+  } else if (zip.length != 5 || isNaN(parseInt(zip))) {
+    alert("ZIP Code must be a number consisting of 5 digits!");
+    return false;
+  } else if (feelings === "") {
+    alert("Feelings cannot be empty!");
+    return false;
+  }
+  return true;
+};
+
 const fetchWeatherTemp = async (baseURL, zip, apiKey) => {
   const url = `${baseURL}?zip=${zip}&appid=${apiKey}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(
+        "fetching temperature from Open Weather Map. Please ensure you entered a correct US ZIP Code!"
+      );
+    }
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    alert(err);
+  }
 };
 
 const postWeatherData = async (url, data) => {
@@ -31,8 +59,9 @@ const postWeatherData = async (url, data) => {
 const updateEntry = async (url) => {
   const response = await fetch(url);
   const data = response.json().then((data) => {
+    const tempInCels = kelvinToCelsius(data.temp);
     document.querySelector("#date").innerHTML = data.date;
-    document.querySelector("#temp").innerHTML = data.temp;
+    document.querySelector("#temp").innerHTML = `${tempInCels} Celsius`;
     document.querySelector("#content").innerHTML = data.userResponse;
   });
 };
@@ -41,9 +70,12 @@ const onClickHandler = () => {
   const zipCode = document.querySelector("#zip").value;
   const feelings = document.querySelector("#feelings").value;
 
+  //Validate inputs and if returns false (invalid), immediately return
+  if (!validateInputs(zipCode, feelings)) {
+    return;
+  }
   fetchWeatherTemp(BASE_URL, zipCode, API_KEY)
     .then((data) => {
-      console.log(data);
       return postWeatherData("/data", {
         temp: data.main.temp,
         date: date,
@@ -53,8 +85,10 @@ const onClickHandler = () => {
     .then(() => {
       updateEntry("/data");
     })
-    .catch((err) => {
-      console.log(err);
+    .catch(() => {
+      alert(
+        "Error: Could not post data to server nor update the UI since the temperature could not be fetched from Open Weather Map."
+      );
     });
 };
 
